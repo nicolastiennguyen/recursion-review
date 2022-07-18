b1f952dd5ea0c2939a82e7ed7f0020f62ffb8b17
Jessie Zhao <jcz11213@gmail.com>
2022-07-18 13:59:01 -0700

    work on JSONstringify up thru object handling

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,51 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  console.log('Input: ', obj, 'type: ', typeof obj)
+  // if input object is a number, convert to a string
+  if (typeof obj === 'number') {
+    return obj.toString();
+  }
+  // if input is null, return null
+  if (obj === null) {
+    return 'null';
+  }
+  // if input is a boolean, convert to a string
+  if (typeof obj === 'boolean') {
+    return obj.toString();
+  }
+  // if input is string return the same input w double quotes
+
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  // for arrays
+  if (Array.isArray(obj)) {
+    // if empty
+    if (obj.length === 0) {
+      return '[]';
+    }
+    var temp = [];
+    // [1,2,3]
+    obj.forEach(function(item) {
+      temp.push(stringifyJSON(item));
+    })
+    return '[' + temp + ']';
+  }
+  // for objects
+  if (typeof obj === 'object') {
+    // if empty
+    if (Object.keys(obj).length === 0) {
+      return '{}';
+    }
+    var temp  = {};
+    for (var key in obj) {
+      var stringKey = stringifyJSON(key);
+      temp[stringKey] = stringifyJSON(obj[key]);
+    }
+    return temp;
+
+  }
+
+
 };
